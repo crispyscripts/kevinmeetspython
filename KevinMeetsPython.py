5c5107ad4dfa5a936c276d519d2e3502b7486d72
extrusion <danielstilly@gmail.com>
2019-12-24 09:39:38 +0000

    first commit

---
+++ KevinMeetsPython.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+
+adminConnectionString = 'mongodb+srv://crisp:C7e7d95DBLrWiHyL@kevin-8xfyp.azure.mongodb.net/test?retryWrites=true&w=majority'
+
+client = MongoClient(adminConnectionString)
+db=client.admin
+# Issue the serverStatus command and print the results
+serverStatusResult=db.command("serverStatus")
+pprint(serverStatusResult)
+
+
